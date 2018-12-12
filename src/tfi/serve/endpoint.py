0d6d7acdab5667db7408727570b59555a2b93d50
Adam Bouhenguel <adam@bouhenguel.com>
2018-12-12 10:38:04 -0800

    Support for specific parameter as "raw" HTTP body
    In some cases, we want to perform an inference against
    an image directly. We'd like to avoid needing to wrap
    the image in a larger structure.
    
    To specify which "input" the body corresponds to, set the
    x-tesserai-input-body header to the name of an input.
    
    TFI will decode the body according to its mimetype
    and use its decoded value as the specified input.
    
    If there are additional inputs you'd like to specify,
    set the x-tesserai-input-json header to a JSON-encoded
    object. Its fields will be combined with field defined
    by the body.

--- src/tfi/serve/endpoint.py
+++ src/tfi/serve/endpoint.py
@@ -15,23 +15,76 @@ from tfi.base import _recursive_transform
 
 from collections import OrderedDict
 
+from tfi.tensor.codec import encode as _encode_tensor
 
-def _decode_request(req):
-  if req.form is not None:
-    decoded = OrderedDict()
-    for field, value in req.form.items():
-      ch = value[0]
-      if ch != '{' and ch != '[' and ch != '"' and not value.isdecimal():
-        # HACK(adamb) Replace value with a json-encoded version of itself
-        value = json.dumps(value)
-      decoded[field] = tfi_data.json(value)
+def _decode_form_value(value):
+  # Attempt to auto-detect JSON-encoded values
+  ch = value[0]
+  # Assume that values that start with a char that is 
+  # - non-object,
+  # - non-array,
+  # - non-quote,
+  # - and non-numeric
+  # should be considered a string.
+  if ch != '{' and ch != '[' and ch != '"' and not value.isdecimal():
+    # HACK(adamb) Replace value with a json-encoded version of itself
+    value = json.dumps(value)
+  return tfi_data.json(value)
 
-    for field, file in req.files.items():
-      decoded[field] = tfi_data.file(file, mimetype=file.mimetype)
+
+def _decode_request(request):
+  print("_decode_request", request, request.mimetype)
+
+  decoded = OrderedDict()
+
+  if 'x-tesserai-input-json' in request.headers:
+    decoded.update(json.loads(request.headers['x-tesserai-input-json']))
+
+  if 'x-tesserai-input-body' in request.headers:
+    body_key = request.headers['x-tesserai-input-body']
+    decoded[body_key] = tfi_data.bytes(request.data, mimetype=request.mimetype)
     return decoded
+
+  decoded_json = request.get_json()
+  if decoded_json is not None:
+    decoded.update(decoded_json)
+
+  if request.form is not None:
+    for field in request.form:
+      values = request.form.getlist(field)
+      decoded[field] = [
+        _decode_form_value(value)
+        for value in values
+      ]
+
+      if len(decoded[field]) == 1:
+        decoded[field] = decoded[field][0]
+
+    for field, file in request.files.items():
+      print("field, file", field, file)
+      decoded[field] = tfi_data.file(file, mimetype=file.mimetype)
   
-  if req.get_json() is not None:
-    return req.get_json()
+  return decoded
+
+from tfi.json import as_jsonable as _as_jsonable
+
+def _encode_response(request, result):
+  if 'x-tesserai-output-body' in request.headers:
+    body_key = request.headers['x-tesserai-output-body']
+    if body_key[0] != '[':
+      body_key = [body_key]
+    else:
+      body_key = json.loads(body_key)
+
+    for key in body_key:
+      result = result[key]
+
+  accept = request.headers['accept']
+  if '*/*' in accept or 'application/json' in accept:
+    jsonable = _as_jsonable(result)
+    return 'application/json', jsonify(jsonable)
+
+  return accept, _encode_tensor({accept: lambda x: x}, result)
 
 def _get_request_field(req, field, annotation):
   if field not in req:
@@ -41,8 +94,6 @@ def _get_request_field(req, field, annotation):
 
 def _maybe_plural(n, singular, plural):
   return singular if n == 1 else plural
-
-from tfi.json import as_jsonable as _as_jsonable
 
 def _MissingParameters(missing):
   noun = _maybe_plural(len(missing), "parameter", "parameters")
@@ -70,9 +121,9 @@ def make_endpoint(model, method_name):
       raise _MissingParameters(missing)
     
     result = method(**d)
-    jsonable = _as_jsonable(result)
-    
-    response = make_response(jsonify(jsonable), 200)
-
+    mimetype, encoded = _encode_response(request, result)
+    response = make_response(encoded, 200)
+    response.headers['Content-Type'] = mimetype
     return response
+
   return fn
